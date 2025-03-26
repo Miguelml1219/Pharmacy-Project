ec0f0dd9e8e9575856368196b4e5d9b39a3d0c03
Santiagovs7 <villadasantasantiago208@gmail.com>
2025-03-26 00:25:46 -0500

    carpeta

---
+++ src/Pharmacy_Project/network/socket_client/Socket_ClientGUI.java
@@ -0,0 +1,11 @@
+package Pharmacy_Project.network.socket_client;
+
+import javax.swing.*;
+
+public class Socket_ClientGUI {
+    private JPanel main;
+    private JTextArea textArea1;
+    private JTextField textField1;
+    private JButton exitButton;
+    private JButton sendMessageButton;
+}
---
+++ src/Pharmacy_Project/network/socket_server/Socket_ServerGUI.java
@@ -0,0 +1,11 @@
+package Pharmacy_Project.network.socket_server;
+
+import javax.swing.*;
+
+public class Socket_ServerGUI {
+    private JPanel main;
+    private JTextArea textArea1;
+    private JButton sendMessageButton;
+    private JTextField textField1;
+    private JButton exitButton;
+}
